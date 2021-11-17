91bd95a3c25593222197b0672bc12170940716ab
Haval-Ahmed <Haval_Sadik_Ahmed@outlook.com>
2021-11-17 14:22:47 -0800

    Added pthread dependency and removed some glx extensions

--- Engine/Core/Window/X11Window.cpp
+++ Engine/Core/Window/X11Window.cpp
@@ -109,13 +109,13 @@ namespace ForerunnerEngine
                     GLX_DRAWABLE_TYPE   , GLX_WINDOW_BIT,
                     GLX_RENDER_TYPE     , GLX_RGBA_BIT,
                     GLX_X_VISUAL_TYPE   , GLX_TRUE_COLOR,
+                    GLX_DOUBLEBUFFER    , True,
                     GLX_RED_SIZE        , 8,
                     GLX_GREEN_SIZE      , 8,
                     GLX_BLUE_SIZE       , 8,
                     GLX_ALPHA_SIZE      , 8,
                     GLX_DEPTH_SIZE      , 24,
                     GLX_STENCIL_SIZE    , 8,
-                    GLX_DOUBLEBUFFER    , True,
                     None
                 };
 
